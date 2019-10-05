e9dbd5dbe9d9bb0c220ba6e03efb1541d2ab18d4
Aaditya das <imaadityadas@gmail.com>
2019-10-05 10:06:37 +0530

    Update authRoutes.js

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -10,7 +10,7 @@ router.post("/signup", async (req, res) => {
   try {
     const user = new User({ email, password });
     await user.save();
-    const token = jwt.sign({ userid: user._id }, "MY_SECRET_KEY");
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
     res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
